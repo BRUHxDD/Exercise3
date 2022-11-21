f198308c5e532b8959bac0d9806e0d29248d7ce6
Teuku Muhammad Attar Fadhilah <teuku.muhammad.ft21@mail.umy.ac.id>
2022-11-21 15:54:09 +0700

    add function for public bool Search

--- Exercise3/Program.cs
+++ Exercise3/Program.cs
@@ -22,7 +22,15 @@ namespace Exercise_Linked_List_D
         }
         public bool Search(int rollNo, ref Node previous, ref Node current)/*Searches for the specified node*/
         {
-            
+            for (previous = current = LAST.next; current != LAST; previous = current, current = current.next)
+            {
+                if (rollNo == previous.rollNumber)
+                    return (true);/*returns true if the node is found*/
+            }
+            if (rollNo == LAST.rollNumber)/*If the node is present at the end*/
+                return true;
+            else
+                return (false);/*returns false if the node is not found*/
         }
     }
 
